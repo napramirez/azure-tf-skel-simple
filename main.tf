f56f715f070dfa78a6f9353466ec1e93ef4a90ff
Nap Ramirez <napramirez@gmail.com>
2018-08-21 17:25:59 +0800

    Bumped Terraform and AzureRM versions

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  version = "0.11.1"
+  version = "0.11.8"
 }
 
 provider "azurerm" {
-  version = "1.0.0"
+  version = "1.13.0"
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
